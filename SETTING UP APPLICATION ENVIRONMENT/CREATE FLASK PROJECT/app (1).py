fbdbaa8dcf25c53d584dbdbcf164d21248657a25
Pranita1805 <114275712+Pranita1805@users.noreply.github.com>
2022-11-17 02:06:46 -0600

    Add files via upload

---
+++ SETTING UP APPLICATION ENVIRONMENT/CREATE FLASK PROJECT/app (1).py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import ibm_db
+import re
+
+app = Flask(__name__)
+  
+app.secret_key = 'a'
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32733;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=bsb19147;PWD=M8Q6aCGQuLHwiHkU",'','')
+
+@app.route('/')
+
+def homer():
+    return render_template('home.html')
+
+
+@app.route('/login',methods =['GET', 'POST'])
+def login():
+    global userid
+    msg = ''
+   
+  
+    if request.method == 'POST' :
+        username = request.form['username']
+        password = request.form['password']
+        sql = "SELECT * FROM users WHERE username =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print (account)
+        if account:
+            session['logged']
